bfc42784107ed0daa0a7ed8912b573e2fe4b1dac
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-21 17:42:41 +0800

    Week-5 day-1 first exercise modified.

--- src/simpleGoldenAcornApp.js
+++ src/simpleGoldenAcornApp.js
@@ -8,6 +8,8 @@ class SimpleGoldenAcornApp extends React.Component {
     this.state = {
       value: 0,
     };
+    this.eatOne = this.eatOne.bind(this);
+    this.buyOne = this.buyOne.bind(this);
   }
 
   componentDidMount() {
@@ -18,6 +20,16 @@ class SimpleGoldenAcornApp extends React.Component {
         this.changeValue(-1)();
       }
     });
+  }
+
+  buyOne() {
+    this.setState(prevState => ({ value: prevState.value + 1 }));
+  }
+
+  eatOne() {
+    this.setState(prevState => (prevState.value - 1 > -1
+      ? { value: prevState - 1 }
+      : { value: prevState.value }));
   }
 
   changeValue(count) {
@@ -33,7 +45,8 @@ class SimpleGoldenAcornApp extends React.Component {
     const { value } = this.state;
     return (
       <div>
-        <Button name="Buy One" callback={this.changeValue(1)} />
+        {/* <Button name="Buy One" callback={this.changeValue(1)} /> */}
+        <Button name="Buy One" callback={this.buyOne} />
         <Display>
           {value}
         </Display>
