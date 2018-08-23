f1631fd17eb0002af98ea7c7be008294d78f2cc4
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-23 15:12:43 +0800

    Week-5 Day-4 task 4 router modified(without redux arrow key).

--- src/containers/BuyAcorn/index.js
+++ src/containers/BuyAcorn/index.js
@@ -2,24 +2,18 @@ import { connect } from 'react-redux';
 import { buy } from '../../actions';
 import Button from '../../components/Button';
 
-const mapStateToProps = () => {
-  return {
-    name: 'Buy One',
-  };
-};
+const mapStateToProps = () => ({ name: 'Buy One' });
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    callback: function callback() {
+const mapDispatchToProps = dispatch => ({
+  callback: function callback() {
+    dispatch(buy(1));
+  },
+  keyHandler: function keyHandler(e) {
+    if (e.keyCode === 38) {
       dispatch(buy(1));
-    },
-    keyHandler: function keyHandler(e) {
-      if (e.keyCode === 38) {
-        dispatch(buy(1));
-      }
-    },
-  };
-};
+    }
+  },
+});
 
 const BuyAcorn = connect(
   mapStateToProps,
